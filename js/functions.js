8a84a8b5b7103a1de887fab91c8858532e4267f5
archangel9800 <pain_s@mail.ru>
2019-08-21 11:51:46 +0300

    2019.08.21 cart modal done

--- js/functions.js
+++ js/functions.js
@@ -377,6 +377,29 @@ centeredSlides: true,
     
     
 
+$(".modalbox").fancybox();
+ 
+     onmouseout="this.blur();"
+    $('.modalbox').click(function(){
+        console.log('1');
+        $('.homeModals input').blur();    
+    })
+    
+    
+    
+//$(document).ready(function(){
+//    $(".modalbox").fancybox();
+//    $("#f_contact").submit(function(){ return false; });
+//    $("#f_send").on("click", function(){
+//         
+//        // тут дальнейшие действия по обработке формы
+//        // закрываем окно, как правило делать это нужно после обработки данных
+//        $("#f_contact").fadeOut("fast", function(){
+//            $(this).before("<p><strong>Ваше сообщение отправлено!</strong></p>");
+//            setTimeout("$.fancybox.close()", 1000);
+//        });
+//    });
+//});
     
 });
  

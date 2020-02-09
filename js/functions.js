eae3fbcf03f7b3ecb9da47b5bd951454c0d823a9
Ivan <doctrmanhattan@gmail.com>
2020-02-09 10:54:43 +0200

    12

--- js/functions.js
+++ js/functions.js
@@ -2,33 +2,11 @@
  * js/functions.js
  * Copyright (c) ТОВ "Елпрофи", 2019 All rights reserved.
  * Шаблон разработали Левковец И.В., Броновицкий А.Н., 2019. archangel, b1te
- * 29.01.2020
+ * 04.02.2020
  */
 
 
 $(document).ready(function () {
-
-   
-    
-/* Sample function that returns boolean in case the browser is Internet Explorer*/
-  
-    
-//function isIE() {
-//  ua = navigator.userAgent;
-//  /* MSIE used to detect old browsers and Trident used to newer ones*/
-//  var is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
-//  return is_ie; 
-//}
-//    
-///* Create an alert to show if the browser is IE or not */
-//if (isIE()){
-//    alert('It is InternetExplorer');
-//}else{
-//    alert('It is NOT InternetExplorer');
-//}    
-    
-
-    
 ///.topSearch script start
 $(".topSearch").on('mouseenter', function (e){;
     var element = $(this);
@@ -41,41 +19,7 @@ $(".topSearch").on('mouseenter', function (e){;
     })
   });     
 ///.topSearch script end  
-    
-///#smSearch script start
-//function offerSearchResTopPos(){
-//    var firstTopLine = $('#firstTopLine');
-//    var firstNavLine = $('#firstNavLine');
-//    var smSearch = $('#smSearch');
-//    var offerSearchRes = $('#smSearch.topSearch .offerSearchRes');
-//    offerSearchRes.css({
-//       top: firstNavLine.position().top + firstTopLine.height() + 'px',
-//       left: smSearch.offset().left + 'px',
-//       width: smSearch.width() + 'px'
-//    });
-//};
-//if($(window).width() < 992) {
-//   offerSearchResTopPos(); 
-//};
-//$(window).resize(function(){  
-//    if($(window).width() < 992) {
-//        offerSearchResTopPos(); 
-//    };
-//}); 
-//$(window).scroll(function(){  
-//    if($(window).width() < 992) {
-//       offerSearchResTopPos(); 
-//    };
-//});  
-//$('header').scroll(function(){
-//    if($(window).width() < 992) {  
-//        offerSearchResTopPos();
-//        
-//    };
-//});    
-///#smSearch script end      
-    
-    
+  
 ///.sticky-top script start
     if($(window).width() < 992) {
        $('.sticky-top').css({
@@ -98,80 +42,7 @@ $(".topSearch").on('mouseenter', function (e){;
         }; 
    });    
 ///.sticky-top script end  
-    
- ///#callUsDropdown script start
-//    $("#callUsDropdown").on("click", function (event) {
-////        event.preventDefault();
-//        function DropDown(el) {
-//            this.dd = el;
-//            this.placeholder = this.dd.find('p > span');
-//            this.opts = this.dd.find('ul > li');
-//            this.val = '';
-//            this.index = -1;
-//            this.initEvents();
-//        }
-//        DropDown.prototype = {
-//            initEvents : function() {
-//                var obj = this;
-//
-//                obj.opts.on('click',function(){
-//                    var opt = $(this);
-//                    obj.val = opt.html();
-//                    obj.index = opt.index();
-//                    obj.placeholder.html(obj.val); $(this).parents().find('#callUsDropdown').removeClass('visibleBl');
-//                });
-//                    
-//            },
-//            getValue : function() {
-//                return this.val;
-//            },
-//            getIndex : function() {
-//                return this.index;
-//            }
-//        }
-//        $(function() {
-//            var dd = new DropDown( $('#callUsDropdown') );  
-//        });
-////            $(this).removeClass('visibleBl');
-//        
-//        
-//    });  
-       ///#callUsDropdown script end 
-
-    
-    
-    
-    
-
-    
-
-//        $('.cssmenu li.active').addClass('open').children('ul').show();
-//            $('.cssmenu li.has-sub>span').on('click', function(){
-//                $(this).removeAttr('href');
-//                var element = $(this).parent('li');
-//                
-//                $(document).mouseup(function (e){ 
-//                    if (!element.is(e.target) && element.has(e.target).length === 0) { // проверка условия если клик не по его дочерним элементам
-//                        element.removeClass('open');
-//                        element.find('li').removeClass('open');
-//                        element.find('ul').slideUp(200);
-//                    }
-//                })
-//                if (element.hasClass('open')) {
-//                    element.removeClass('open');
-//                    element.find('li').removeClass('open');
-//                    element.find('ul').slideUp(200);
-//                }
-//                else {
-//                    element.addClass('open');
-//                    element.children('ul').slideDown(200);
-//                    element.siblings('li').children('ul').slideUp(200);
-//                    element.siblings('li').removeClass('open');
-//                    element.siblings('li').find('li').removeClass('open');
-//                    element.siblings('li').find('ul').slideUp(200);
-//                }
-//            });
-
+  
     $(document).mouseup(function (e){ // отслеживаем событие клика по веб-документу
         var block = $("#block"); // определяем элемент, к которому будем применять условия (можем указывать ID, класс либо любой другой идентификатор элемента)
         if (!block.is(e.target) // проверка условия если клик был не по нашему блоку
@@ -180,11 +51,6 @@ $(".topSearch").on('mouseenter', function (e){;
         }
     });
 
-
-    
-    
-
-    
 //.textLimit script start
   function textLimit(){  
     $(".textLimit").text(function(i, text) { 
@@ -205,31 +71,18 @@ $(".topSearch").on('mouseenter', function (e){;
   }
   textLimit();
 //.textLimit script end
-    
-
-//    $('aside .customZ51Checkbox > .dropdown-menu').click(function(e) {
-//        e.stopPropagation();
-//    });
-//    $('aside .customZ51Checkbox > p').click(function(e) {
-//        $('aside .customZ51Checkbox > [aria-expanded="false"]').parent().addClass('show');
-//        $('aside .customZ51Checkbox > p').not(this).sibling().find('.dropdown-menu').addClass('show');
-//        
-//    });
-//        $(document).click(function(e) {
-//        console.log($(this)[0])
-//    })
-//    
-    //aside .asideDropdown script start
-    $('.customDrop > p, .customDrop .dropItem').click(function(e) {
-        $(this).parent().toggleClass('visibleBl');
-    });
-    $(document).mouseup(function (e){ 
-        var block = $(".hideClickNotThisDrop");
-        if (!block.is(e.target) && block.has(e.target).length === 0) { 
-            block.removeClass('visibleBl');
-        }
-    });
-    //aside .asideDropdown script end
+ 
+//aside .asideDropdown script start
+$('.customDrop > p, .customDrop .dropItem').click(function(e) {
+    $(this).parent().toggleClass('visibleBl');
+});
+$(document).mouseup(function (e){ 
+    var block = $(".hideClickNotThisDrop");
+    if (!block.is(e.target) && block.has(e.target).length === 0) { 
+        block.removeClass('visibleBl');
+    }
+});
+//aside .asideDropdown script end
     
 //.showMore script start
 //    простой скрипт на открытие соседних элементов(исп в фильтрах)
@@ -259,9 +112,7 @@ $('.hidddenContInside .showCont').click(function(e) {
     $(this).addClass('dispNone');
 })
 //.hidddenContInside .showMore script end    
-
-    
-    
+ 
 //.switchBlock .switch script start
 //    скрипт закрытия открытия контента по кнопке (используется для добавления коментариев и ворпросов к товару)
 $('.switchBlock .switch').click(function(e) {
@@ -272,25 +123,7 @@ $('.switchBlock .switch').click(function(e) {
     $(showClass).removeClass('dispNone');
 })
 //.switchBlock .switch script end   
-    
-
-    
-//    var values= $( ".priceVal #buy_price" ).attr('value').split(', ')
-//        $( "input[name=price_s]" ).val(  values[ 0 ] ); // выводим  значение от при запуске
-//        $( "input[name=price_f]" ).val(  values[ 1 ] ); // выводим  значение до при запуске
-//        $("#buy_price").slider({
-//            range: true,
-//            min: parseInt($( "#buy_price" ).attr('min')), // минимальное значение цены
-//            max: parseInt($( "#buy_price" ).attr('max')), // максимальное значение цены
-//            step: parseInt($( "#buy_price" ).attr('step')), // шаг слайдераs
-//            values: $( "#buy_price" ).attr('value').split(', '),  // начальные значения - позиции ползунков на шкале
-//            slide: function( event, ui ) {
-//                $( "input[name=price_s]" ).val(  ui.values[ 0 ] ); // выводим  значение от при изменении
-//                $( "input[name=price_f]" ).val(  ui.values[ 1 ] ); // выводим  значение до при изменении
-//            }
-//        });
-
-
+  
 //aside .filter-int.filter-slider script start
 $(".filter-int.filter-slider .filter-slider-val").slider({});
 var filter_slider_timer;
@@ -334,34 +167,13 @@ $(".filter-int.filter-slider .filter-slider-min, .filter-int.filter-slider .filt
     });
 //.btnNav script end   
 
-    
-//      function includerHtml(){
-//    var includes = $('[data-include]');
-//    jQuery.each(includes, function(){
-//      var file = 'views/' + $(this).data('include') + '.html';
-//      $(this).load(file);
-//    });
-//  };
-//  includerHtml(); 
-    
-
-    
-
-//    $("a.galleryModal").fancybox({
-//		'transitionIn'	:	'elastic',
-//		'transitionOut'	:	'elastic',
-//		'speedIn'		:	600, 
-//		'speedOut'		:	200, 
-//		'overlayShow'	:	false
-//	});
-
 ///.swiperAnalog script start  
 function goodsCarouselInit(){
   var swiper = new Swiper('.swiper-container.swiperAnalog', {
       slidesPerView: 5,
       spaceBetween: 20,
-//       centeredSlides: true,
-      // init: false,
+      loop: true,
+      allowTouchMove: false,
       navigation: {
         nextEl: '.swiperAnalog .swiper-button-next',
         prevEl: '.swiperAnalog .swiper-button-prev',
@@ -399,8 +211,8 @@ $(window).resize(function(){
 ///.swiperVideo script start  
 function videoCarouselInit(){
   var swiper = new Swiper('.swiper-container.swiperVideo', {
-//       centeredSlides: true,
-      // init: false,
+    loop: true,
+    allowTouchMove: false,
       navigation: {
         nextEl: '.swiperVideo .swiper-button-next',
         prevEl: '.swiperVideo .swiper-button-prev',
@@ -414,6 +226,7 @@ videoCarouselInit();
 var swiper = new Swiper('.swiper-container.swiperIndex', {
     centeredSlides: true,
     loop: true,
+    allowTouchMove: false,
           autoplay: {
             delay: 5000,
             disableOnInteraction: false,
@@ -432,27 +245,6 @@ var swiper = new Swiper('.swiper-container.swiperIndex', {
 ///.modals in home activation script start 
 $(".modalbox").fancybox();
 ///.modals in home activation script end     
-
-//    onmouseout="this.blur();"
-//    $('.modalbox').click(function(){
-//    $('.homeModals input').blur();    
-//})    
-    
-    
-//$(document).ready(function(){
-//    $(".modalbox").fancybox();
-//    $("#f_contact").submit(function(){ return false; });
-//    $("#f_send").on("click", function(){
-//         
-//        // тут дальнейшие действия по обработке формы
-//        // закрываем окно, как правило делать это нужно после обработки данных
-//        $("#f_contact").fadeOut("fast", function(){
-//            $(this).before("<p><strong>Ваше сообщение отправлено!</strong></p>");
-//            setTimeout("$.fancybox.close()", 1000);
-//        });
-//    });
-//});
-    
     
 $(".modalbox").fancybox("#inSuccess");    
 //     $('#inSuccess').fancybox().trigger('click'); 
@@ -644,16 +436,7 @@ $('.topSearch input').bind("keyup", function() {
                         out += '<div class="d-flex flex-wrap">';
                         out += '<span class="offerProductName col-12 pr-0 mb-1">' + this.label + '</span>';
                         out += '<div class="offerProductStatus col-12 pr-0">';
-                        out += '<p class="m-0">Цена: <span>' + this.price + '</span></p>';
-//                        if (this.enabled <= 0) {
-//                            out += '<p class="m-0">Цена: <span>' + this.price + '.</span> На складе: <span>Нет.</span></p>';
-//                        } else if (this.enabled == 1) {
-//                            out += '<p class="m-0">Цена: <span>' + this.price + '.</span> На складе: <span>Заканчивается.</span></p>';
-//                        } else if (this.enabled > 1 && this.enabled <= 4) {
-//                            out += '<p class="m-0">Цена: <span>' + this.price + '.</span> На складе: <span>Мало.</span></p>';
-//                        } else {
-//                            out += '<p class="m-0">Цена: <span>' + this.price + '.</span> На складе: <span>Много.</span></p>';
-//                        }
+                        out += '<p class="m-0">Цена: <span class="mr-1">' + this.price + '</span>' + (this.date_income!='' ? (' ' + this.date_income) : '') + '</p>';
                         out += '</div></div></a></li>';
                     });
                     $('.topSearch .offerSearchRes').html(out).toggleClass('visibleBl', true);
@@ -1046,13 +829,6 @@ if($('#orderConfirmationForm').length){
 
 }
 //order confirmation end
-
-
-
-///adress autocomplite script start
-
-///dress autocomplite end script
-
 
 ///.deliveryTime script start
 if($('.deliveryTime')){
